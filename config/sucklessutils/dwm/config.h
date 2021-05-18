c41b37f2e95e3b914bee5ca1dd0437fb8fe5c276
dconixDev <dconixdev@protonmail.com>
2021-05-18 22:43:10 +0000

    update

--- config/sucklessutils/dwm/config.h
+++ config/sucklessutils/dwm/config.h
@@ -38,11 +38,12 @@ static const Rule rules[] = {
 	{ "Sxiv",     NULL,       NULL,       0,            1,           1,           -1 },
 	{ "st",       NULL,       NULL,       0,            1,           0,           -1 },
 	{ "mpv",      NULL,       NULL,       1 << 3,       0,           0,           -1 },
+	{ "Brave",    NULL,       NULL,       1 << 8,       0,           0,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           0,           -1 },
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.5;  /*0.55;  factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
@@ -76,8 +77,8 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,             XK_f, 	   spawn,          SHCMD("firefox") },
-	{ MODKEY|ShiftMask,             XK_v, 	   spawn,          SHCMD("firefox -P discord 'https://discord.com/app'") },
+	{ MODKEY|ShiftMask,             XK_f, 	   spawn,          SHCMD("brave") },
+	{ MODKEY|ShiftMask,             XK_v, 	   spawn,          SHCMD("brave https://discord.com/app") },
 	{ MODKEY,                       XK_q,      spawn,          SHCMD("pmenu") },
 	{ MODKEY|ShiftMask,             XK_a,      spawn,          SHCMD("amenu") },
 	{ MODKEY|ShiftMask,             XK_x,      spawn,          SHCMD("xkill") },
